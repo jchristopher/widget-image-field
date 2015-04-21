4860c3a25ad3e309b5b32f4f22ce4123858a0b62
Jonathan Christopher <jonathan@mondaybynoon.com>
2015-04-21 00:18:02 -0400

    Brought up to speed with .org repo, updated for 4.2 check

---
+++ readme.txt
@@ -0,0 +1,39 @@
+=== Widget Image Field ===
+Contributors: jchristopher
+Donate link: http://mondaybynoon.com/donate/
+Tags: adopt-me, widget, image, field, upload
+Requires at least: 3.3
+Tested up to: 4.2
+Stable tag: 0.3
+
+[Adopt Me!] Creates an image field for use in your own Widgets
+
+== Description ==
+
+**Adopt me!** This plugin uses Thickbox which has been deprecated in favor of the new Media enhancements brought about in WordPress 3.5. If you're interested in adopting this plugin (it is no longer actively developed) please let me know.
+
+Please see [this article](http://mondaybynoon.com/20120206/wordpress-widget-image-field/)
+
+== Installation ==
+
+1. Upload the `widget-image-field` folder to the `/wp-content/plugins/` directory
+1. Activate the plugin through the 'Plugins' menu in WordPress
+1. Integrate with your Widget(s)
+
+== Frequently Asked Questions ==
+
+= I activated the plugin, but don't see a new Widget =
+
+Please see [this article](http://mondaybynoon.com/20120206/wordpress-widget-image-field/)
+
+== Screenshots ==
+
+1. The provided image field in use
+
+== Changelog ==
+
+= 0.3 =
+* Size definition was not working (props github.com/etiennetremel)
+
+= 0.2 =
+* Initial release
--- widget-image-field.php
+++ widget-image-field.php
@@ -1,23 +1,17 @@
 <?php
 /*
-    Plugin Name: Widget Image Field
-    Plugin URI: http://mondaybynoon.com/wordpress/widget-image-field/
-    Description: This prepares an image field for use within your own Widgets
-    Author: Jonathan Christopher
-    Version: 0.3
-    Author URI: http://mondaybynoon.com/
+Plugin Name: Widget Image Field
+Plugin URI: http://mondaybynoon.com/wordpress/widget-image-field/
+Description: This prepares an image field for use within your own Widgets
+Author: Jonathan Christopher
+Version: 0.3
+Author URI: http://mondaybynoon.com/
 */
 
-if( !defined( 'IS_ADMIN' ) )
-    define( 'IS_ADMIN',  is_admin() );
-
-define( 'WIDGET_IMAGE_FIELD_VERSION', '0.3' );
-define( 'WIDGET_IMAGE_FIELD_DIR', WP_PLUGIN_DIR . '/' . basename( dirname( __FILE__ ) ) );
 define( 'WIDGET_IMAGE_FIELD_URL', rtrim( plugin_dir_url( __FILE__ ), '/' ) );
 
+class WidgetImageField {
 
-class WidgetImageField
-{
     private $image_id;
     private $src;
     private $width;
@@ -25,14 +19,12 @@ class WidgetImageField
     private $widget_field;
     private $widget = null;
 
-    function __construct( $widget = null, $image_id = 0 )
-    {
-        $uri        = isset( $_SERVER['REQUEST_URI'] ) ? $_SERVER['REQUEST_URI'] : NULL ;
+    function __construct( $widget = null, $image_id = 0 ) {
+        $uri        = isset( $_SERVER['REQUEST_URI'] ) ? $_SERVER['REQUEST_URI'] : NULL;
         $file       = basename( parse_url( $uri, PHP_URL_PATH ) );
 
         // if we're on the Widgets page
-        if( $uri && in_array( $file, array( 'widgets.php' ) ) && IS_ADMIN )
-        {
+        if ( $uri && in_array( $file, array( 'widgets.php' ) ) && is_admin() ) {
             wp_enqueue_script( 'jquery-ui-core' );
             wp_enqueue_script( 'thickbox' );
             wp_enqueue_script( 'media-upload' );
@@ -44,34 +36,31 @@ class WidgetImageField
 
         // set our properties
         $this->widget = $widget;
-        if( $image_id )
-        {
+
+        if ( $image_id ) {
             $this->image_id         = intval( $image_id );
             $this->src              = $this->get_image_src( $this->image_id );
             $this->width            = $this->get_image_width( $this->image_id );
             $this->height           = $this->get_image_height( $this->image_id );
             $this->widget_field     = $this->get_widget_field( $widget, $this->image_id );
         }
+
     }
 
-    function get_image( $size = 'thumbnail' )
-    {
+    function get_image( $size = 'thumbnail' ) {
         $image = false;
 
-        if( $this->image_id )
-        {
+        if ( $this->image_id ) {
             $image = wp_get_attachment_image_src( $this->image_id, $size );
         }
 
         return $image;
     }
 
-    function get_image_src( $size = 'thumbnail' )
-    {
+    function get_image_src( $size = 'thumbnail' ) {
         $src = false;
 
-        if( $this->image_id )
-        {
+        if ( $this->image_id ) {
             $image      = $this->get_image( $size );
             $src        = $image[0];
         }
@@ -79,12 +68,10 @@ class WidgetImageField
         return $src;
     }
 
-    function get_image_dimensions( $size = 'thumbnail' )
-    {
+    function get_image_dimensions( $size = 'thumbnail' ) {
         $dimensions = array( null, null );
 
-        if( $this->image_id )
-        {
+        if ( $this->image_id ) {
             $image          = $this->get_image( $size );
             $dimensions     = array( $image[1], $image[2] );
         }
@@ -92,12 +79,10 @@ class WidgetImageField
         return $dimensions;
     }
 
-    function get_image_width( $size = 'thumbnail' )
-    {
+    function get_image_width( $size = 'thumbnail' ) {
         $width = false;
 
-        if( $this->image_id )
-        {
+        if ( $this->image_id ) {
             $dimensions     = $this->get_image_dimensions( $size );
             $width          = $dimensions[0];
         }
@@ -105,12 +90,10 @@ class WidgetImageField
         return $width;
     }
 
-    function get_image_height( $size = 'thumbnail' )
-    {
+    function get_image_height( $size = 'thumbnail' ) {
         $height = false;
 
-        if( $this->image_id )
-        {
+        if ( $this->image_id ) {
             $dimensions     = $this->get_image_dimensions( $size );
             $height         = $dimensions[1];
         }
@@ -118,17 +101,14 @@ class WidgetImageField
         return $height;
     }
 
-    function get_widget_field( $field_name = null )
-    {
+    function get_widget_field( $field_name = null ) {
         $field = false;
-        if( $this->widget && ( isset( $this->widget->image_field ) || $field_name ) )
-        {
-            $field  = "<div class='iti-image-widget-field'><div class='iti-image-widget-image' id='" . $this->widget->id . "'>";
-            $field .= "<input type='hidden' style='display:none;' id='" . $this->widget->get_field_id( $this->widget->image_field ) . "' name='" . $this->widget->get_field_name( $this->widget->image_field ) . "' value='" . $this->image_id . "' />";
+        if ( $this->widget && ( isset( $this->widget->image_field ) || $field_name ) ) {
+            $field  = "<div class='iti-image-widget-field'><div class='iti-image-widget-image' id='" . esc_attr( $this->widget->id ) . "'>";
+            $field .= "<input type='hidden' style='display:none;' id='" . esc_attr( $this->widget->get_field_id( $this->widget->image_field ) ) . "' name='" . esc_attr( $this->widget->get_field_name( $this->widget->image_field ) ) . "' value='" . esc_attr( $this->image_id ) . "' />";
 
-            if( $this->image_id )
-            {
-                $field .= "<img src='" . $this->src . "' width='" . $this->width . "' height='" . $this->height . "' />";
+            if ( $this->image_id ) {
+                $field .= "<img src='" . esc_url( $this->src ) . "' width='" . absint( $this->width ) . "' height='" . absint( $this->height ) . "' />";
             }
 
             $field .= "</div>";
